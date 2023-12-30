8215d6c8fb75768c07dc4d24c1ee1763824e62f8
feryalHisham <fargalaxy24@gmail.com>
2023-12-30 23:01:02 +0200

    implement forgot password steps

--- src/app/services/forget-password.service.ts
+++ src/app/services/forget-password.service.ts
@@ -19,7 +19,9 @@ export class ForgetPasswordService {
   verifyCode(email: string, code: string) {
     const params = new HttpParams().append('email', email).append('code', code);
     return this.http.post(`${this.url}/verifyCode`, null, {params});
+  }
 
-
+  resetPassword(email: string, password: string) {
+    return this.http.patch(`${this.url}/resetPassword`, {email, password});
   }
 }
