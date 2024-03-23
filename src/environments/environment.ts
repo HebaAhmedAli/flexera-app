5a2502cf80c150b20b371cf628da235d22373c14
HebaAhmedAli <haali@ejada.com>
2024-03-23 23:46:45 +0200

    fix.

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   secretKeyStorage: '47d382fedd3b6c8375736fefa1143883',
-  baseUrl: 'http://api.thetdsapp.com',
+  baseUrl: 'https://api.thetdsapp.com',
   apiKey: 'AIzaSyAsWA3MRAhgxtl-hewuePJ0Rgdlegg_cTg',
   role: 'USER_ROLE'
 };
