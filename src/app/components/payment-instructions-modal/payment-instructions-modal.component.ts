9fb1116d53392974826c99bf79c4a38b07290dad
HebaAhmedAli <haali@ejada.com>
2024-05-10 13:39:14 +0300

    Fix numbers as sys parameters.

--- src/app/components/payment-instructions-modal/payment-instructions-modal.component.ts
+++ src/app/components/payment-instructions-modal/payment-instructions-modal.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { ModalController } from '@ionic/angular';
+import { SystemParametersService } from 'src/app/services/system-parameters.service';
 
 @Component({
   selector: 'app-payment-instructions-modal',
@@ -11,22 +12,26 @@ export class PaymentInstructionsModalComponent  implements OnInit {
   @Input() paymentMethod?: string;
   @Input() totalPrice?: string;
 
-  number = '011334499559';
+  number = '';
   title = '';
-  
-  constructor(private modalController: ModalController) { }
+
+  constructor(private modalController: ModalController, private sysParamServ: SystemParametersService) { }
 
   ngOnInit() {
+
 
     switch(this.paymentMethod) {
       case 'instapay':
         this.title = 'Pay via InstaPay';
+        this.number = this.sysParamServ.getValue('INSTAPAY') as string;
         break;
       case 'vodafone-cash':
         this.title = 'Pay via Voafone Cash';
+        this.number = this.sysParamServ.getValue('VODAFONE-CASH') as string;
         break;
       case 'etisalat-cash':
         this.title = 'Pay via Etisalat Cash';
+        this.number = this.sysParamServ.getValue('ETISALAT-CASH') as string;
     }
 
   }
